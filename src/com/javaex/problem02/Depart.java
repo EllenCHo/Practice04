d236b2eb194a37e243778b926bb4bf83e744dc46
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:25:26 +0900

    Problem02 finish

--- src/com/javaex/problem02/Depart.java
+++ src/com/javaex/problem02/Depart.java
@@ -3,5 +3,17 @@ package com.javaex.problem02;
 public class Depart extends Employee {
 
     //코드작성
+	String depart;
+	
+	public Depart() {}
+	
+	public Depart(String name, int salary, String depart) {
+		super(name, salary);
+		this.depart = depart;
+	}
+	
+	public void getInformation() {
+		System.out.println("이름:"+super.getName()+"  연봉:"+super.getSalary()+"  부서:"+depart);
+	}
 
 }
