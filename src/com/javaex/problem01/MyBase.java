dac83e3226c9159e897880bb96a0f361dab598b4
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:21:40 +0900

    Problem01 finish

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -3,5 +3,21 @@ package com.javaex.problem01;
 public class MyBase extends Base{
 
     //코드작성
+	public void service(String state) {
+
+        if (state.equals("낮")) {
+            day();
+        } else if (state.equals("밤")){
+            night();
+        } else {
+        	afternoon();
+        }
+    }
+	
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+	}
+
+    
     
 }
