c103090b66529fb1d03f865626267cced9eae8c4
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:39:16 +0900

    Problem04 finish

--- src/com/javaex/problem04/Bird.java
+++ src/com/javaex/problem04/Bird.java
@@ -2,6 +2,30 @@ package com.javaex.problem04;
 
 public abstract class Bird {
     private String name;
+    
+    public Bird() {}
+    
+    public Bird(String name) {
+    	this.name = name;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    protected String getName() {
+    	return name;
+    }
+    
+    public abstract void sing();
+    
+    public abstract void fly();
+    
+    public void showName() {
+    	System.out.println("새의 이름은 "+name+"입니다.");
+    }
+    
+    
 
 
 
