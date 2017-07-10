72fb712581f08574183e69bf7aadf00069ed3618
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:56:20 +0900

    Problem04 수정

--- src/com/javaex/problem04/Duck.java
+++ src/com/javaex/problem04/Duck.java
@@ -4,15 +4,15 @@ public class Duck extends Bird {
 	public Duck() {}
 
     public void sing() {
-    	System.out.println("오리("+super.getName()+")가 소리내어 웁니다.");
+    	System.out.println("오리("+getName()+")가 소리내어 웁니다.");
     }
 
     public void fly() {
-    	System.out.println("오리("+super.getName()+")가 날지 않습니다.");
+    	System.out.println("오리("+getName()+")가 날지 않습니다.");
     }
     
     public void showName() {
-    	System.out.println("오리의 이릅은 "+super.getName()+" 입니다.");
+    	System.out.println("오리의 이릅은 "+getName()+" 입니다.");
     }
 
 }
--- src/com/javaex/problem04/Sparrow.java
+++ src/com/javaex/problem04/Sparrow.java
@@ -5,15 +5,15 @@ public class Sparrow extends Bird {
 	public Sparrow() {}
 
     public void sing() {
-    	System.out.println("참새("+super.getName()+")가 소리내어 웁니다.");
+    	System.out.println("참새("+getName()+")가 소리내어 웁니다.");
     }
 
     public void fly() {
-    	System.out.println("참새("+super.getName()+")가 날아 다닙니다.");
+    	System.out.println("참새("+getName()+")가 날아 다닙니다.");
     }
     
     public void showName() {
-    	System.out.println("참새의 이릅은 "+super.getName()+" 입니다.");
+    	System.out.println("참새의 이릅은 "+getName()+" 입니다.");
     }
 
 }
